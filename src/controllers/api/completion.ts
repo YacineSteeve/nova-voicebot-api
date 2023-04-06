12b7b35e250da361d3127e9100b168d476483c97
Yacine BOUKARI <steeveboukari9@gmail.com>
2023-04-06 19:20:21 +0200

    Add a controller factory and improve security layers (#41)

--- src/controllers/api/completion.ts
+++ src/controllers/api/completion.ts
@@ -1,24 +1,13 @@
-import type { Request, Response } from 'express';
+import type { Request } from 'express';
+import createControllerMethod from '@controllers/controller';
 import apiService from '@services/api';
 
-export async function getCompletion(request: Request, response: Response) {
-    request.accepts('application/json');
-    response.type('application/json');
-
-    try {
-        const completionResponse = await apiService.completion(
-            {
-                prompt: request.body.prompt,
-                user: request.body.user,
-            },
-            request.get('authorization')?.substring(7),
-        );
-
-        response.status(completionResponse.status).json(completionResponse.data);
-    } catch (error) {
-        response.status(500).json({
-            success: false,
-            error: 'ERROR: ' + error.message,
-        });
-    }
-}
+export const getCompletion = createControllerMethod(async (request: Request) => {
+    return await apiService.completion(
+        {
+            prompt: request.body.prompt,
+            user: request.body.user,
+        },
+        request.get('authorization')?.substring(7),
+    )
+});
