f4f06d6b63531e1ff571f298a939d339aa8c9991
Yacine BOUKARI <steeveboukari9@gmail.com>
2023-04-06 18:43:54 +0200

    feat: improve security

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import type { Express } from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
 import morgan from 'morgan';
 import { apiRouter, userRouter, supportRouter } from './routes';
 import { authorizeApiRequest } from './middlewares';
@@ -8,11 +9,14 @@ import { authorizeApiRequest } from './middlewares';
 const app: Express = express();
 
 app.use(cors());
+app.use(helmet());
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
+
+app.disable('x-powered-by');
 
 app.get('/', (req, res) => {
     res.send('Welcome to the NOVA API!');
